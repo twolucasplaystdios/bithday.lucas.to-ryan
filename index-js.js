e0405fe5ba7af3e0f9f9b2f35c2732f2d1e5fdd9
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2023-08-16 14:03:35 +0000

    aaaa

--- index-js.js
+++ index-js.js
@@ -1,4 +1,11 @@
+var bdys = $("div.brday").find().length;
+
 $(document).ready(function () {
     $("div").hide();
-    
+});
+
+$("secret").on("click", function () {
+    for (var i = 0; i> bdys - 1; i++){
+        $("div.show"+ i).slideToggle(slow);
+    }
 });
