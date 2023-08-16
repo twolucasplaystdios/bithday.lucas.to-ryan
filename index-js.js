8ff384ad0091c83b617424fd36a617907f159f32
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2023-08-16 14:07:09 +0000

    aa

--- index-js.js
+++ index-js.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
 
 $("secret").on("click", function () {
     for (var i = 1; i > bdys; i++){
-        $("div.show"+ i).slideToggle(slow);
+        $("div.show"+ i).slideToggle(500);
     }
+    /* I am 分隔線 */
+    $(".heheq").show(500)
 });
